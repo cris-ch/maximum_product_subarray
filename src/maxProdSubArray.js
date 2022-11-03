06568e58d2a89d5247dad4a804ecdf8132ea6426
Cristian <cristian.christensen@gmail.com>
2022-11-03 17:07:31 +0100

    Refactored code to make edge test pass.

--- src/maxProdSubArray.js
+++ src/maxProdSubArray.js
@@ -2,18 +2,20 @@ const maxProdSubArray = (nums) => {
   let max = nums[0];
   let min = nums[0];
   let result = nums[0];
-
   for (let i = 1; i < nums.length; i++) {
     // update max dependind on 3 possible cases
       // 1. number(+) it self is the largest
       // 2. number(+) * max(+) is the largest
       // 3. number(-) * min(-) is the largest 
-    max = Math.max(nums[i], nums[i] * max, nums[i] * min);
+    curMax = Math.max(nums[i], nums[i] * max, nums[i] * min);
     // update min dependind on 3 possible cases
       // 1. number(-) it self is the smallest
       // 2. number(-) * max(+) is the smallest
       // 3. number(-) * min(-) is the smallest
-    min = Math.min(nums[i], nums[i] * max, nums[i] * min);
+    curMin = Math.min(nums[i], nums[i] * max, nums[i] * min);
+    // update min and max with the min and max from this iteration (curMax and curMin)
+    max = curMax;
+    min = curMin;
     // update result
     result = Math.max(result, max);
   }
--- test/maxProdSubArray.test.js
+++ test/maxProdSubArray.test.js
@@ -15,14 +15,17 @@ describe("maxProdSubArray", () => {
 
   it("returns the maximum product of a subarray", () => {
     expect(maxProdSubArray([1, -3, 5])).toEqual(5);
-  } );
+  });
 
   it("returns the maximum product of a subarray", () => {
     expect(maxProdSubArray([1, -3, 5, 7])).toEqual(35);
-  } );
+  });
 
-  it("returns the maximum product of a subarray", () => { 
+  it("returns the maximum product of a subarray", () => {
     expect(maxProdSubArray([1, -3, 5, 7, -2])).toEqual(210);
-  } );
+  });
 
+  it("returns the maximum product of a subarray", () => {
+    expect(maxProdSubArray([-4, -3, -2])).toEqual(12);
+  });
 });
